5b92fa33bac25e26997b9f2bc2892362586c074d
xzalerr <zalecki.raf@gmail.com>
2024-11-19 12:24:23 +0100

    Secret key secured

--- bazy_danych/bazy_danych/settings.py
+++ bazy_danych/bazy_danych/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(env_file=BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+8bzq^t51tx_+rrydg56a=@=h5sx37rxq5rp4mhcabl9a39--'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
